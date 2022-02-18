fc19fcb6bd41bcd55600fd8f1dcdf6b1d52f9697
Sumaiyashe <sumaiyashompa137@gmail.com>
2022-02-18 21:52:55 +0600

    code commit

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ function ExpensesValueConvert(number) {
     const input = document.getElementById(number).value;
     const amount = parseFloat(input);
     //clear input field
+    document.getElementById(number).value = "";
 
     return amount;
 }
@@ -12,31 +13,42 @@ document.getElementById('calculate-button').addEventListener("click", function (
     const foodExpenses = ExpensesValueConvert('food-input');
     const rentExpenses = ExpensesValueConvert('rent-input');
     const clothExpenses = ExpensesValueConvert('cloth-input');
+    if (foodExpenses >= 0 && rentExpenses >= 0 && clothExpenses >= 0) {
+        const totalAmount = foodExpenses + rentExpenses + clothExpenses;
 
-    const totalAmount = foodExpenses + rentExpenses + clothExpenses;
-
-    const incomeInput = document.getElementById('income-input').value;
+        const incomeInput = document.getElementById('income-input').value;
 
 
-    // get and update total expenses
+        // get and update total expenses
+        if (incomeInput > totalAmount) {
+            const expensesTotal = document.getElementById('total-expenses');
+            const expenseTotalText = expensesTotal.innerText;
+            const previousExpensesTotal = parseFloat(expenseTotalText);
 
-    const expensesTotal = document.getElementById('total-result');
-    const expenseTotalText = expensesTotal.innerText;
-    const previousExpensesTotal = parseFloat(expenseTotalText);
+            const balanceTotal = document.getElementById('total-balance');
+            const balanceTotalText = parseFloat(balanceTotal.innerText);
+            const previousBalanceTotal = parseFloat(balanceTotalText);
 
-    const balanceTotal = document.getElementById('total-balance');
-    const balanceTotalText = parseFloat(balanceTotal.innerText);
-    const previousBalanceTotal = parseFloat(balanceTotalText);
+            expensesTotal.innerText = previousExpensesTotal + totalAmount;
+            let newBalance = incomeInput - totalAmount;
+            balanceTotal.innerText = previousBalanceTotal + newBalance;
+        }
+        else {
+            alert("total amount is grater than income amount");
+        }
+    }
 
-    expensesTotal.innerText = previousExpensesTotal + totalAmount;
-    let newBalance = incomeInput - totalAmount;
-    balanceTotal.innerText = previousBalanceTotal + newBalance;
+    else {
+        alert('enter a positive number');
+    }
+
+
 })
 document.getElementById('savings-button').addEventListener('click', function () {
 
     const incomeInput = document.getElementById('income-input').value;
 
-    const saveInput = document.getElementById('save-input ').value;
+    const saveInput = document.getElementById('percentage-input').value;
     const savingPerchantage = parseFloat(saveInput);
 
     //get balance and saving amount
@@ -44,15 +56,28 @@ document.getElementById('savings-button').addEventListener('click', function () 
     const balanceTotal = document.getElementById('total-balance');
     const balanceTotalText = parseFloat(balanceTotal.innerText);
 
-    const balanceRemaining = document.getElementById('remaining-balance');
+    const balanceRemaining = document.getElementById('remaining-balance-text');
+
 
     //get and update saving amount
-    const savingAmount = document.getElementById('saving-text');
+    const savingAmount = document.getElementById('saving-amount-text');
+
+
 
     //calculate perchantage
     var savingscalc = savingPerchantage / 100;
-    const a = savingscalc * incomeInput;
-    savingAmount.innerText = a;
+    const totalAmount = savingscalc * incomeInput;
+    savingAmount.innerText = totalAmount;
     // calculate remaining balance
-    balanceRemaining.innerText = balanceTotalText - a;
+    if (balanceTotalText >= savingAmount.innerText) {
+        balanceRemaining.innerText = balanceTotalText - totalAmount;
+    }
+    else {
+        alert("savings amount limit crossed");
+    }
 })
+
+
+
+
+
